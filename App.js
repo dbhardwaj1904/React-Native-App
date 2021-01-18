d165c4c3247e1c805c005d4177c60eee1b7e65a5
dbhardwaj1904 <bhardwaj.dhanjay1998@gmail.com>
2021-01-18 16:03:54 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,42 +1,39 @@
 import React from 'react';
-import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { TextInput, Card, Button } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
-import { FlatList } from 'react-native-gesture-handler';
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import SearchCity from './components/SearchCity';
-import Home from './components/Home';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as firebase from 'firebase';
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import HomeScreen from './screens/HomeScreen';
+import Loading from './screens/Loading';
+import { firebaseConfig } from './config';
+import VideoCall from './screens/VideoCall';
 
-const Tab = createBottomTabNavigator();
+firebase.initializeApp(firebaseConfig);
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
+    <>
+
     <NavigationContainer>
-      <Tab.Navigator screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+      <Stack.Navigator headerMode="none">
+        <Stack.Screen name="Loading" component={Loading} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="VideoCall" component={VideoCall} />
+      </Stack.Navigator>
 
-            if (route.name === 'Current') {
-              iconName = 'md-cloud';
-            } else if (route.name === 'SelectCity') {
-              iconName = 'md-options';
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'white',
-          inactiveTintColor: 'gray',
-          activeBackgroundColor: '#6200ee',
-          inactiveBackgroundColor: '#6200ee'
-        }}>
-        <Tab.Screen name="Current" component={Home} />
-        <Tab.Screen name="SelectCity" component={SearchCity} />
-      </Tab.Navigator>
+      {/* <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Loading" component={Loading} />
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Signup" component={Signup} />
+          </Stack.Navigator> */}
     </NavigationContainer>
+
+    </>
   );
 }
---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBmH2dXEpJHTSC2I0L_kP3Jgawm34aG-2s",
+    authDomain: "fir-rn-2458f.firebaseapp.com",
+    databaseURL: "https://fir-rn-2458f-default-rtdb.firebaseio.com",
+    projectId: "fir-rn-2458f",
+    storageBucket: "fir-rn-2458f.appspot.com",
+    messagingSenderId: "677657687272",
+    appId: "1:677657687272:web:4840b5e239cce3f142c55d",
+    measurementId: "G-PCTQ211J8Y"
+  };
